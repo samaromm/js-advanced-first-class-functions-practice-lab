c7995b70ef8daa91c219ed7450ccfbb1370f2641
samaromm <samaromm98@gmail.com>
2020-02-11 07:54:49 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function totalRevenue(arr){
+    return arr.reduce(function(total,ele){let rev=ele.revenue;return total+=rev},0)
+}
+
+
+function averageRevenue(arr){
+    let sumRev= arr.reduce(function(total,ele){let rev=ele.revenue;return total+=rev},0)
+    return sumRev/arr.length
+}
+
+function logDriverNames(arr){
+    arr.forEach(function(ele){
+        console.log(ele.name)
+    })
+}
+
+function logDriversByHometown(arr,location){
+    arr.forEach(function(ele){
+        if(ele.hometown==location)console.log(ele.name)
+    })
+}
+
+/*function driversByRevenue(array){ ​
+       let newArray =[]
+        newArray.push(array.sort((a, b)=>{
+          a.revenue - b.revenue
+        }))
+        return newArray
+        return array.slice().sort((a,b)=>a.revenue - b.revenue)
+}*/
+
+function driversByRevenue(drivers) {
+    return drivers.slice().sort(function (a, b) {
+      return a.revenue - b.revenue;
+    })
+}
+
+function driversByName(arr){
+    return arr.slice().sort((a,b)=>a.name.localeCompare(b.name))
+}    
+    
